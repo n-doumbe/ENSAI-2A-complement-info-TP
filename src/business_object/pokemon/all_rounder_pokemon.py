8b3fbca4f7cf1b9679f12b39c8f597305ce82c22
id2824-ensai <ndoumbe.diopn@gmail.com>
2025-09-05 10:44:03 +0000

    modifs des tests

--- src/business_object/pokemon/all_rounder_pokemon.py
+++ src/business_object/pokemon/all_rounder_pokemon.py
@@ -5,8 +5,11 @@ class AllRounderPokemon(AbstractPokemon):
     def __init__(self, stat_max=None, stat_current=None, level=0, name=None, type_pk=None):
         super().__init__(stat_max=None, stat_current=None, level=0, name=None, type_pk=None)
 
+    def get_pokemon_attack_coef(self) -> float:
+        """
+        Compute a damage multiplier related to the pokemon type.
 
-
-
-
-        1 + (self.attack_current + self.defense_current) / 200
+        Returns :
+            float : the multiplier
+        """
+        return 1 + (self.attack_current + self.defense_current) / 200
